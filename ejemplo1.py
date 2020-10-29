6fd05c4bfb7048d9a7273026f27ca05e30b85bb3
Your Name030701Ivan <ivanuribarren@Ivans-MacBook-Pro.local>
2020-10-29 13:33:56 -0600

    nuevas imagnes

--- ejemplo1.py
+++ ejemplo1.py
@@ -1,4 +1,5 @@
 import cv2
+import numpy as np
 
 imagen = cv2.imread('imagen.jpg')
 imagen = cv2.cvtColor(imagen,cv2.COLOR_BGR2RGB)
@@ -16,6 +17,12 @@ imagen[0][1] = 0
 imagen[0][2] = 0
 
 cv2.imwrite('grayimagen.jpg',imagen)
+matriz = np.zeros((256,256),np.float32)
+print(matriz.shape)
+cv2.imwrite('matrizImagen.jpg',matriz)
+imagen = cv2.cvtColor(matriz,cv2.COLOR_GRAY2BGR)
+print(imagen.shape)
+cv2.imwrite('matrizColorImagen.jpg',imagen)
 
 #cv2.imwrite('resizeImagen.jpg',imagen)
 #cv2.imshow('image',imagen)
